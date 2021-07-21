e34c2dcc64f8f9eddaccdfe1702a47132aaebe22
KenInoue1911 <inouetkrk3@gamail.com>
2021-07-21 20:20:43 +0900

    first

---
+++ paiza/java入門編1/変数を使えるようにしよう2.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		String player1 = "勇者";
+		System.out.println(player1 + "の体力が回復した");
+	}
+}
---
+++ paiza/java入門編1/変数を使えるようにしよう3.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		String team = "勇者と戦士";
+		System.out.println(team + "の体力が回復した");
+	}
+}
