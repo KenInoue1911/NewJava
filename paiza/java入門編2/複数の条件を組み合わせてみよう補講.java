c750f7fd594450098dd5f611313b4ee02a450210
KenInoue1911 <inouetkrk3@gamail.com>
2021-07-24 21:01:17 +0900

    7/24

---
+++ paiza/java入門編2/複数の条件を組み合わせてみよう補講.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(Stirng[] args) {
+  int discomfort = (int)(Math.random() * 71 + 30);
+  System.out.println("不快指数は" + discomfort + "です");
+
+ if (discomfort <= 55 || discomfort >=75) {
+  System.out.println("不快です")
+}
+}
+}
